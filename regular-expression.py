42e8c1452f3050b2699036e0513c84b16f46f4aa
Ganesan <gansenator@gmail.com>
2017-10-17 06:54:38 +0530

    Added regular expression to search for thousand and 100 fields of Rommon varaible

--- regular-expression.py
+++ regular-expression.py
@@ -1,2 +1,11 @@
 
 #Program for Regular expression to check the entered ROMMON Variable to correct
+
+import re
+rommon_number = input_raw("Enter the Rommon Number you want to check")
+
+#Pattern for Rommon varaible check
+
+patter = '''^                    # Starting with
+           M{0,3}
+           (D?C{0,3}|CD|DC)      # Checking For 100 tp 900
