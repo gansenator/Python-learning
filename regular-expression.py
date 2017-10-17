393505c91009d56e1a6358335a2ffa30616d6207
Ganesan <gansenator@gmail.com>
2017-10-17 11:17:06 +0530

    modified to get rid of syntax and new line issues

--- regular-expression.py
+++ regular-expression.py
@@ -1,20 +1,19 @@
 
 #Program for Regular expression to check the entered ROMMON Variable to correct
-While(1):
- import re
- rommon_number = input_raw("Enter the Rommon Number you want to check")
+import re
+rommon_number = raw_input("Enter the Rommon Number you want to check\n")
 
 #Pattern for Rommon varaible check
 
- patter = '''^                    # Starting with
+pattern = '''^                    # Starting with
            M{0,3}                # Checking for thousands
            (D?C{0,3}|CD|CM)      # Checking For 100 to 900
            (L?X{0,3}|XL|XC)      # Checking for 10 to 90
            (V?I{0,3}|IV|IX)      # Checking for 1 to 9
            $                     # End of pattern 
            '''
- if re.match(pattern,rommon_number,re.VERBOSE):
-           print "Correct Rommon Number Entered'
- else:
-           print "please enter Correct Rommon Number"
+if re.match(pattern,rommon_number,re.VERBOSE):
+          print "Correct Rommon Number Entered"
+else:
+          print "please enter Correct Rommon Number"
 
